d357c3720e83a86a07c143886ea13327cf6d0eab
Humberto Galdino <humberto.galdino@live.com>
2024-10-15 20:05:02 -0300

    Finishing Back-End

--- src/produto/produto.controller.ts
+++ src/produto/produto.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { Produto } from 'src/core';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Produto } from '@gstore/core';
 import { ProdutoPrisma } from './produto.prisma';
 
 @Controller('produtos')
 export class ProdutoController {
   constructor(readonly repo: ProdutoPrisma) {}
+
+  @Post()
+  async salvarProduto(@Body() produto: Produto): Promise<void> {
+    return this.repo.salvar(produto);
+  }
 
   @Get()
   async obterProdutos(): Promise<Produto[]> {
@@ -14,5 +19,10 @@ export class ProdutoController {
   @Get(':id')
   async obterProdutoPorId(@Param('id') id: string): Promise<Produto | null> {
     return this.repo.obterPorId(Number(id));
+  }
+
+  @Delete(':id')
+  async excluirProduto(@Param('id') id: string): Promise<void> {
+    return this.repo.excluir(+id);
   }
 }
--- src/produto/produto.prisma.ts
+++ src/produto/produto.prisma.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { Produto } from '@prisma/client';
+import { Produto } from '@gstore/core';
 import { PrismaProvider } from 'src/db/prisma.provider';
 
 @Injectable()
 export class ProdutoPrisma {
   constructor(readonly repo: PrismaProvider) {}
+
+  async salvar(produto: Produto): Promise<void> {
+    await this.repo.produto.upsert({
+      where: { id: produto.id ?? -1 },
+      update: produto,
+      create: produto,
+    });
+  }
 
   async obter(): Promise<Produto[]> {
     return this.repo.produto.findMany() as any;
@@ -13,5 +21,9 @@ export class ProdutoPrisma {
   async obterPorId(id: number): Promise<Produto | null> {
     const produto = await this.repo.produto.findUnique({ where: { id } });
     return (produto as any) || null;
+  }
+
+  async excluir(id: number): Promise<void> {
+    await this.repo.produto.delete({ where: { id } });
   }
 }
